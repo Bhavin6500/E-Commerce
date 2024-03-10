d86cef4be1049e822daa086a86a4851dc6f15716
Bhavin <bhj@gmail.com>
2024-03-09 20:00:37 -0500

    third

--- src/features/product/productListAPI.js
+++ src/features/product/productListAPI.js
@@ -1,9 +1,37 @@
 // A mock function to mimic making an async request for data
-export function fetchCount(amount = 1) {
-  return new Promise((resolve) =>{
+export function fetchAllProducts() {
+  return new Promise(async(resolve) =>{
   // we have written by our own
+  const response = await fetch("http://localhost:8080/products")
+  const data = await response.json()
+  resolve({data})
+  }
+  );
+}
 
+
+export function fetchProductsByFilters(filter) {
+ //filter ={"category":"smartphone"}
+  let queryString = "";
+  for(let key in filter){
+    const categoryValues = filter[key];
+  queryString += `${key}=${categoryValues}&`;
+  }
+ // queryString += `${key}=${sort[key]}&`;
+
+  return new Promise(async(resolve) =>{
+  // we have written by our own
+  const response = await fetch("http://localhost:8080/products?"+queryString);
+  console.log(response)
+  const data = await response.json()
+  console.log(data)
+  resolve({data})
+  console.log(resolve(data))
 
   }
   );
 }
+
+
+
+
--- src/features/product/productListSlice.js
+++ src/features/product/productListSlice.js
@@ -1,21 +1,35 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { fetchCount } from './productListAPI';
+import { fetchAllProducts, fetchProductsByFilters } from './productListAPI';
 
 const initialState = {
-  value: 0,
+  products: [],
   status: 'idle',
 };
 
-export const incrementAsync = createAsyncThunk(
-  'counter/fetchCount',
-  async (amount) => {
-    const response = await fetchCount(amount);
+export const fetchAllProductsAsync = createAsyncThunk(
+  'product/fetchAllProducts',
+  async () => {
+    const response = await fetchAllProducts();
    return response.data;
   }
 );
 
-export const counterSlice = createSlice({
-  name: 'counter',
+
+
+///by creating the filter api suppose if we select the smartphone then it will display only smartphone
+export const fetchProductsByFiltersAsync = createAsyncThunk(
+  'product/fetchProductsByFilters',
+  async (filter) => {
+    const response = await fetchProductsByFilters(filter);
+   return response.data;
+  }
+);
+
+
+
+
+export const productSlice = createSlice({
+  name: 'product',
   initialState,
   reducers: {
     increment: (state) => {
@@ -26,19 +40,26 @@ export const counterSlice = createSlice({
   
   extraReducers: (builder) => {
     builder
-      .addCase(incrementAsync.pending, (state) => {
+      .addCase(fetchAllProductsAsync.pending, (state) => {
         state.status = 'loading';
       })
-      .addCase(incrementAsync.fulfilled, (state, action) => {
+      .addCase(fetchAllProductsAsync.fulfilled, (state, action) => {
         state.status = 'idle';
-        state.value += action.payload;
+        state.products = action.payload;
+      })
+      .addCase(fetchProductsByFiltersAsync.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchProductsByFiltersAsync.fulfilled, (state, action) => {
+        state.status = 'idle';
+        state.products = action.payload;
       });
   },
 });
 
-export const { increment } = counterSlice.actions;
+export const { increment } = productSlice.actions;
 
-export const selectCount = (state) => state.counter.value;
+export const selectAllProducts = (state) => state.product.products;
 
 
-export default counterSlice.reducer;
+export default productSlice.reducer;
